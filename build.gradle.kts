c234b9257753a1e92a06031f8816be23c3ed797e
vood <igor-vood@gmail.com>
2025-05-20 17:42:25 +0300

    d

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,11 @@
+val jimmerVersion by extra { "0.9.46" }
+val springBootVersion by extra { "3.4.5" }
+
+allprojects {
+    group = "ru.vood.jummer"
+    version = jimmerVersion
+}
+
 plugins {
     kotlin("jvm")
 }
--- jummer_1/build.gradle.kts
+++ jummer_1/build.gradle.kts
@@ -1,12 +1,14 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm")
+    id("com.google.devtools.ksp") version "2.1.10-1.0.29"
 //    kotlin("plugin.spring")
 //    id("org.springframework.boot")
 //    id("io.spring.dependency-management")
 }
 
-group = "ru.vood.flow"
-version = "0.0.1-SNAPSHOT"
+val jimmerVersion: String by rootProject.extra
 
 java {
     toolchain {
@@ -20,23 +22,26 @@ repositories {
 
 dependencies {
 
-    // https://mvnrepository.com/artifact/io.arrow-kt/arrow-core
-    implementation(platform("io.arrow-kt:arrow-stack:2.1.1"))
-    implementation(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.10.2"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-    implementation("org.springframework:spring-web:6.2.5")
-    implementation("org.springframework:spring-webflux:6.2.5")
-    api("io.arrow-kt:arrow-core-jvm:2.1.1")
 
-    testImplementation(kotlin("test"))
-//    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.3.7-1.4-M3")
-    testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
-    testImplementation("com.ocadotechnology.gembus:test-arranger:1.6.4")
+    implementation("org.babyfish.jimmer:jimmer-sql-kotlin:${jimmerVersion}")
+    ksp("org.babyfish.jimmer:jimmer-ksp:${jimmerVersion}")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 }
 
+// Without this configuration, gradle command can still run.
+// However, Intellij cannot find the generated source.
 kotlin {
-    compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict")
+    sourceSets.main {
+        kotlin.srcDir("build/generated/ksp/main/kotlin")
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
     }
 }
 
